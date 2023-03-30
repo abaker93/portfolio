ed3d59d05d9da00f47e226121d19fd99d23c5287
Anna Baker <anna.baker.design@gmail.com>
2023-03-30 14:05:47 -0400

    styles page

--- src/routes/Styles.js
+++ src/routes/Styles.js
@@ -1,6 +1,322 @@
 const Styles = () => {
 	return (
-		<h1>Styles</h1>
+		<main template="styles" className="py-5">
+			<div className="container">
+				<div className="row mt-5 mb-2">
+					<div className="col">
+						<h1>Styles</h1>
+					</div>
+				</div>
+
+				<div className="row my-5">
+					<div className="col">
+						<hr />
+					</div>
+				</div>
+				
+				{/* Colors */}
+				<div className="row my-2">
+					<div className="col">
+						<h2>Colors</h2>
+					</div>
+				</div>
+				<div className="row my-2">
+					<div className="col">
+						<h3>Purple</h3>
+					</div>
+				</div>
+				<div className="row g-0 my-2">
+					<div className="col">
+						<div className="text-bg-purple-100 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">purple-100</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-purple-200 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">purple-200</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-purple-300 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">purple-300</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-purple-400 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">purple-400</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-purple-500 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">purple-500</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-purple-600 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">purple-600</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-purple-700 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">purple-700</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-purple-800 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">purple-800</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-purple-900 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">purple-900</p>
+						</div>
+					</div>
+				</div>
+				<div className="row my-2">
+					<div className="col">
+						<h3>Red</h3>
+					</div>
+				</div>
+				<div className="row g-0 my-2">
+					<div className="col">
+						<div className="text-bg-red-100 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">red-100</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-red-200 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">red-200</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-red-300 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">red-300</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-red-400 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">red-400</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-red-500 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">red-500</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-red-600 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">red-600</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-red-700 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">red-700</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-red-800 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">red-800</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-red-900 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">red-900</p>
+						</div>
+					</div>
+				</div>
+				<div className="row my-2">
+					<div className="col">
+						<h3>Cream</h3>
+					</div>
+				</div>
+				<div className="row g-0 my-2">
+					<div className="col">
+						<div className="text-bg-cream-100 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">cream-100</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-cream-200 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">cream-200</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-cream-300 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">cream-300</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-cream-400 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">cream-400</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-cream-500 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">cream-500</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-cream-600 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">cream-600</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-cream-700 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">cream-700</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-cream-800 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">cream-800</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-cream-900 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">cream-900</p>
+						</div>
+					</div>
+				</div>
+				<div className="row my-2">
+					<div className="col">
+						<h3>Grayscale</h3>
+					</div>
+				</div>
+				<div className="row g-0 my-2">
+					<div className="col">
+						<div className="text-bg-100 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">bg-100</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-200 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">bg-200</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-300 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">bg-300</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-400 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">bg-400</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-500 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">bg-500</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-600 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">bg-600</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-700 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">bg-700</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-800 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">bg-800</p>
+						</div>
+					</div>
+					<div className="col">
+						<div className="text-bg-900 p-2 d-flex align-items-center" style={{ minHeight: 100 }}>
+							<p className="m-0">bg-900</p>
+						</div>
+					</div>
+				</div>
+
+				<div className="row my-5">
+					<div className="col">
+						<hr />
+					</div>
+				</div>
+
+				{/* Typography */}
+				<div className="row my-2">
+					<div className="col">
+						<h2>Typography</h2>
+					</div>
+				</div>
+				<div className="row g-3">
+					<div className="col-md-4">
+						<div className="row my-2">
+							<div className="col">
+								<h3 className="border-bottom border-dark">Headings</h3>
+							</div>
+						</div>
+						<div className="row my-2">
+							<h1>Heading 1</h1>
+							<h2>Heading 2</h2>
+							<h3>Heading 3</h3>
+							<h4>Heading 4</h4>
+							<h5>Heading 5</h5>
+							<h6>Heading 6</h6>
+						</div>						
+					</div>
+					<div className="col-md-8">
+						<div className="row my-2">
+						<div className="col">
+								<h3 className="border-bottom border-dark">Display Headings</h3>
+							</div>
+						</div>
+						<div className="row my-2">
+							<h1 className="display-1">Display 1</h1>
+							<h2 className="display-2">Display 2</h2>
+							<h3 className="display-3">Display 3</h3>
+							<h4 className="display-4">Display 4</h4>
+							<h5 className="display-5">Display 5</h5>
+							<h6 className="display-6">Display 6</h6>
+						</div>
+					</div>
+				</div>
+				
+				<div className="row g-3">
+					<div className="col-md-4">
+						<div className="row my-2">
+							<div className="col">
+								<h3 className="border-bottom border-dark">Body - Small</h3>
+							</div>
+						</div>
+						<div className="row my-2">
+							<p className="small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.</p>
+							<p className="small"><strong>Body Bold | lorem ipsum dolor sit amet</strong></p>
+							<p className="small"><em>Body Italic | lorem ipsum dolor sit amet</em></p>
+							<p className="small"><strong><em>Body Bold Italic | lorem ipsum dolor sit amet</em></strong></p>
+						</div>
+					</div>
+					<div className="col-md-4">
+						<div className="row my-2">
+							<div className="col">
+								<h3 className="border-bottom border-dark">Body</h3>
+							</div>
+						</div>
+						<div className="row my-2">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.</p>
+							<p><strong>Body Bold | lorem ipsum dolor sit amet</strong></p>
+							<p><em>Body Italic | lorem ipsum dolor sit amet</em></p>
+							<p><strong><em>Body Bold Italic | lorem ipsum dolor sit amet</em></strong></p>
+						</div>
+					</div>
+					<div className="col-md-4">
+						<div className="row my-2">
+							<div className="col">
+								<h3 className="border-bottom border-dark">Body - Large</h3>
+							</div>
+						</div>
+						<div className="row my-2">
+							<p className="large">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Suspendisse ac rhoncus nisl, eu tempor urna. Curabitur vel bibendum lorem. Morbi convallis convallis diam sit amet lacinia. Aliquam in elementum tellus.</p>
+							<p className="large"><strong>Body Bold | lorem ipsum dolor sit amet</strong></p>
+							<p className="large"><em>Body Italic | lorem ipsum dolor sit amet</em></p>
+							<p className="large"><strong><em>Body Bold Italic | lorem ipsum dolor sit amet</em></strong></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
 	)
 }
 
